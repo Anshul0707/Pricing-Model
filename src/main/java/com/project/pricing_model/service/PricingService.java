48b63d2b22a78f0e0c65a9ef9e05022564a35e29
Anshul0707 <anshul.markwade786@gmail.com>
2023-06-11 19:57:50 +0530

    Service

--- src/main/java/com/project/pricing_model/service/PricingService.java
+++ src/main/java/com/project/pricing_model/service/PricingService.java
@@ -1,7 +1,6 @@
 package com.project.pricing_model.service;
 
 import com.project.pricing_model.model.Card;
-import com.project.pricing_model.model.Subscription;
 
 public interface PricingService {
     public Card getCard();
