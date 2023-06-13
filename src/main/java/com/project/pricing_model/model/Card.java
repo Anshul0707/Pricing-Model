dab745fc7cd859887dc43eb60d8b9c25cc4cc739
Anshul0707 <anshul.markwade786@gmail.com>
2023-06-13 10:58:30 +0530

    reformatted code

--- src/main/java/com/project/pricing_model/model/Card.java
+++ src/main/java/com/project/pricing_model/model/Card.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Data
 public class Card {
     private List<Subscription> subscriptions;
+
     public Card(List<Subscription> subscriptions) {
         this.subscriptions = subscriptions;
     }
